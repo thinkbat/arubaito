73daa8360924f8dac01ba20d5af5662ec710ae8c
Abhay Kumar <abhay.kumar@gartner.com>
2021-06-09 14:18:32 +0530

    replaced org

--- src/arubaito/__mocks__/svgMock.js
+++ src/arubaito/__mocks__/svgMock.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Content = () => <svg />;
 
 // this line mimicks how we import svgs from our token library:
-// example: import { ReactComponent as ErrorSvg } from '@capterra/gdm-design-tokens/assets/icons/alert-error.svg';
+// example: import { ReactComponent as ErrorSvg } from '@thinkbat/gdm-design-tokens/assets/icons/alert-error.svg';
 export const ReactComponent = Content;
 
 export default Content;
