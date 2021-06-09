2b5bc70c00a692ade871cc66b6c59cc1828334aa
GitHub Action <github@thinkbat.com>
2021-06-09 09:15:51 +0000

    Generating styleguide

--- src/arubaito/__mocks__/svgMock.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Content = () => <svg />;
-
-// this line mimicks how we import svgs from our token library:
-// example: import { ReactComponent as ErrorSvg } from '@thinkbat/gdm-design-tokens/assets/icons/alert-error.svg';
-export const ReactComponent = Content;
-
-export default Content;
--- src/arubaito/components/atoms/backdrop/backdrop.examples.md
+++
@@ -1,5 +0,0 @@
-```jsx
-<div className='gdm-relative' style={{height: '400px'}}>
-  <Backdrop onClick={() => console.log('backdrop click')}/>
-</div>
-```
